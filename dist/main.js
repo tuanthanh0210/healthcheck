1fee70edb7e62e059da7b7a4fd28af6110cdbf2a
Thanh Le <justin@192.168.1.2.non-exists.ptr.local>
2025-03-22 04:00:56 +0700

    done

--- dist/main.js
+++ dist/main.js
@@ -3,11 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = require("dotenv");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-dotenv.config();
+dotenv.config({ path: '/secrets/.env' });
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    console.log(process.env);
-    await app.listen(process.env.PORT || 8080);
+    console.log('======================');
+    console.log('======================');
+    console.log('======================');
+    console.log('DB_HOST', process.env.DB_HOST);
+    console.log('DB_PORT', process.env.DB_PORT);
+    console.log('REDIS_URL', process.env.REDIS_URL);
+    console.log('======================');
+    console.log('======================');
+    console.log('======================');
+    await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
 void bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,15 @@ dotenv.config({ path: '/secrets/.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('======================);
-  console.log('======================);
-  console.log('======================);
+  console.log('======================');
+  console.log('======================');
+  console.log('======================');
   console.log('DB_HOST', process.env.DB_HOST);
   console.log('DB_PORT', process.env.DB_PORT);
   console.log('REDIS_URL', process.env.REDIS_URL);
-  console.log('======================);
-  console.log('======================);
-  console.log('======================);
+  console.log('======================');
+  console.log('======================');
+  console.log('======================');
   await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
 void bootstrap();
