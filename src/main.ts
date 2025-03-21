e00b6612ddc32eb351f12db5b4859abe7b52f06d
Justin <54875279+tuanthanh0210@users.noreply.github.com>
2025-03-22 03:10:35 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env);
+  console.log(process.env.DB_HOST);
+  console.log(process.env.DB_PORT);
+  console.log(process.env.REDIS_URL);
   await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
 void bootstrap();
