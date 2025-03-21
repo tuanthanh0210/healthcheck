27183d5da425b9593ef725cc403f19b8aca25fbd
Justin <54875279+tuanthanh0210@users.noreply.github.com>
2025-03-21 23:25:39 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config(); // Chỉ load file khi chạy local
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
